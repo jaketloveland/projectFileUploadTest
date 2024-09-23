9f789b76e2209ad3ea6a5c59c896247a58d13d62
Jake Loveland <jaketloveland@gmail.com>
2024-09-23 15:02:54 +0100

    file upload function working locally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from "react";
-
 import "./App.css";
+import axios from "axios";
+//const express = require("cors");
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [file, setFile] = useState();
+  const [message, setMessage] = useState("");
 
-  return <div></div>;
+  const upload = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    axios
+      .post("http://localhost:3001/upload", formData)
+      .then((result) => {
+        setMessage("file uploaded successfully");
+        setFile(null);
+      })
+      .catch((err) => {
+        setMessage("error uploading file");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }}
+      />
+      <button type="button" onClick={upload}>
+        Upload
+      </button>
+      {message && <p> {message} </p>}
+    </div>
+  );
 }
 
 export default App;
